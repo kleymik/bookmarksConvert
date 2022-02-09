6b1154c1da95bddcdfa3e66b08603ec4402db0c9
Mike Kleyn <michfkl@btinternet.com>
2022-02-09 14:52:35 +0000

    use path objects; hairy html traversal; lxml rather than BeautifulSoup; stringIO for in-memory scan & clean files

--- bmksConvert.py
+++ bmksConvert.py
@@ -6,6 +6,7 @@ from io import StringIO
 from lxml import etree
 from see import see
 from datetime import datetime as dt
+from pathlib import Path
 
 def cleanupDt(f, sio):
     with open(f, 'r') as infile:
@@ -18,51 +19,71 @@ def cleanupDt(f, sio):
 def unixEpochToIsoDateTime(unixIntDateAsStr):
     return dt.fromtimestamp(int(unixIntDateAsStr)).strftime('%Y-%m-%dT%H:%M:%S')
 
+def dftFh(el, fldrPath="/tmp/bmc", wetRun=False):             # depth first traversal create fh (=file hierarchy)
 
-def dftFh(el, fldrPath="/tmp/bmc", wetRun=False):             # depth first traversal
-    indent = [" ","*"][0]
-    depth = (len(fldrPath) - len("/home/kleyn/projects/bookmarkMerge/testArea"))//2
+    if not wetRun: outFile=sys.stdout
     for e in el.getchildren():                                # ['href', 'add_date', 'last_modified', 'icon_uri', 'icon', 'last_charset']
 
-        if e.tag=='dd' or (e.tag=='a' and e.text):
-            if e.tag=='a' and e.text:
+        # print("DEBUG", e.tag, e.text, e.get('href'), e.keys()) # debug
+
+        if (e.tag=='dd') or (e.tag=='a' and e.text and (e.get('add_date') or e.get('last_visit')) and not e.get('href').startswith('find')): # and not e.get('href').startswith('ftp')
+            if e.tag=='a':
                 try:
-                    pageCleanName = re.sub(r"[^a-zA-Z0-9_\ ]", "", e.text).strip().replace(" ","_")
-                    if wetRun: outFile = open(fldPth+"/"+pageCleanName+'.url', 'w')
-                    else:      outFile=sys.stdout
-                    if True:
-                        print(pageCleanName)
-                        print(                          "TITLE::",         e.text,                                         file=outFile)
+                    pageCleanName = re.sub(r"[^a-zA-Z0-9_\ ]", "", e.text).strip().replace(" ","_")[:200]   # limit file name size
+                    urlFileName = fldrPath+"/"+pageCleanName+'.url'
+                    urlMtime = int(e.get('add_date') or e.get('last_visit'))
+                    if wetRun: outFile = open(urlFileName, 'w')
+                    print(pageCleanName)
+                    if False:
+                        print(                          "TITLE:",          e.text,                                         file=outFile)
                         print(                          "URI:",            e.get('href'),                                  file=outFile)
                         print(                          "DATE_ADDED:",     unixEpochToIsoDateTime(e.get('add_date')),      file=outFile)
                         if e.get('last_modified'): print("DATE_MODIFIED:", unixEpochToIsoDateTime(e.get('last_modified')), file=outFile)
                         if e.get('icon_uri'):      print("ICON_URI:",      e.get('icon_uri'),                              file=outFile)
                         if e.get('icon'):          print("ICON:",          e.get('icon'),                                  file=outFile)
                         if e.get('last_charset'):  print("LAST_CHARSET:",  e.get('last_charset'),                          file=outFile)
-                except:
+
+                except Exception as e:
+                    print(e)
                     print("FAILED ON", e.text)
                     print(e.keys())
                     if wetRun: outFile.close()
                     sys.exit()
-            elif e.tag=='dd' and e.text:                      # sometimes (rarely but it happens, at '<T> is followed by adecriptive <dd> that should also be written into the file
+            elif e.tag=='dd' and e.text:                      # sometimes (rarely but it happens, at <DT> is followed by adecriptive <DD> that should also be written into the file
                 print("DESCRIPTION:", e.text, file=outFile)
-            if wetRun: outFile.close()
-        else:
-            if e.tag=='h3':                                   # create a subfolder
-                fldrCleanName = re.sub(r"[^a-zA-Z0-9_\ ]", "", e.text).strip().replace(" ","_")
-                print(fldrCleanName)
-                fldrPath += '/'+fldrCleanName
-                print(f"os.mkdir({fldrPath})")
-            elif e.tag not in ['dl','p','dt']:
-                print("OTHER", e.tag, file=sys.stderr)
 
-        dftFh(e, fldrPath=fldrPath)
+            #print("DEBUG:Closing")
+            if wetRun:
+                print("Closing", urlFileName, urlMtime, unixEpochToIsoDateTime(str(urlMtime)))
+                outFile.close()
+                stat = os.stat(urlFileName)                  # times must have two ints or floats (unix timestamps): (atime, mtime)
+                os.utime(urlFileName, times=(stat.st_atime, urlMtime))
+
+        elif e.tag=='h3':                                   # create a subfolder
+
+            fldrCleanName = re.sub(r"[^a-zA-Z0-9_\ ]", "", e.text).strip().replace(" ","_")
+            print(fldrCleanName)
+            fldrPath += '/'+fldrCleanName
+            print(f"os.mkdir({fldrPath})")
+            if wetRun:
+                os.makedirs(fldrPath,exist_ok=True)
+
+        #elif e.tag not in ['dl','p','dt']:
+
+            # print("OTHER-TAG ", e.tag, e.text, e.keys(), file=sys.stderr)
+
+        else:
+
+            print("OTHER", e.tag, e.text, e.keys(), file=sys.stderr)
+            for k in e.keys(): print("TAG", k, e.get(k), file=sys.stderr)
+
+        dftFh(e, fldrPath=fldrPath, wetRun=wetRun)
 
 
 def dftPrint(el, depth=0):                                    # depth first traversal - make file hierarchy
     indent = ["   ","*"][0]
     for e in el.getchildren():                                # ['href', 'add_date', 'last_modified', 'icon_uri', 'icon', 'last_charset']
-        if   e.tag=='a':
+        if e.tag=='a':
             pageCleanName = re.sub(r"[^a-zA-Z0-9_\ ]", "", e.text).strip().replace(" ","_")
             print(indent*depth, pageCleanName, e.get('href')) # print(indent*depth, e.tag, e.get('href'), e.text)
         elif e.tag=='h3':
@@ -76,24 +97,25 @@ def dftPrint(el, depth=0):                                    # depth first trav
 
 
 print("len(sys.argv)=", len(sys.argv), file=sys.stderr)
-testArea = "/home/kleyn/projects/bookmarkMerge/testArea"
-wetRun = False
+
 if len(sys.argv)<2:
     print("Usage: bmTraverse.py bookmarks.html [folderWriteArea]")
 else:
-    sio = cleanupDt(sys.argv[1], StringIO(''))
+    testArea    = "/home/kleyn/projects/bookmarkMerge/testArea"
+    wetRun      = False
+    inFile      = Path(sys.argv[1])
+
+    sio = cleanupDt(inFile, StringIO(''))             # parse cleaned-up file using lxml eTree
     tr = etree.parse(sio, etree.HTMLParser())
     sio.close()
-    if len(sys.argv)==3:
-        topFolder=sys.argv[2]
-        if wetRun: os.mkdir(testArea+'/'+toFolder)
-        dftFh(tr.getroot(), fldrPath=testArea+"/"+topFolder, wetRun=False)
-    else:
-        dftPrint(tr.getroot())
+
+    if len(sys.argv)==3 and sys.argv[2]=='W': wetRun = True
+    if wetRun: os.makedirs(testArea+'/'+inFile.stem, exist_ok=True)
+    dftFh(tr.getroot(), fldrPath=testArea+"/"+inFile.stem, wetRun=wetRun)
+
+#else:        dftPrint(tr.getroot())
 
 
-#    soup = BeautifulSoup(htmlFd)
-#    print(soup.prettify())
 #if __name__ == "__main__":
 # a https://wiki.python.org/jython/
 # {'href': 'https://wiki.python.org/jython/',
