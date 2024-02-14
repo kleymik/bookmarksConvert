21d3433df9b127004b2398214a401078354cca3f
Mike Kleyn <michfkl@btinternet.com>
2024-02-14 21:40:28 +0000

    glob to specify a list of url-files to open in browser

--- openUrlFile.py
+++ openUrlFile.py
@@ -17,6 +17,7 @@ import configparser                    # for .url, .desktop
 import plistlib                        # for .webloc xml
 import subprocess
 import datetime
+from glob import glob
 
 logDir        = f"{os.path.expanduser('~')}/history"
 chosenBrowser = ['xdg-open', 'firefox', 'chromium', 'konqueror'][1]  # could add command line options, such as --new-tab
@@ -60,43 +61,47 @@ if __name__ == "__main__":
         description='open url embedded in file',
         epilog='-------- openUrlFile.py --------')
 
-    parser.add_argument('file|folder path')                       # positional argument
-    parser.add_argument('-t', '--type',       action='store_true')   # type out the file containing the url
-    parser.add_argument('-u', '--url',        action='store_true')   # print the url
-    parser.add_argument('-v', '--verbose',    action='store_true')   # be verbose
-    parser.add_argument('-bf', '--firefox',   action='store_true')   # browse with firefox
-    parser.add_argument('-bc', '--chromium',  action='store_true')   # browse with chromiumT
-    parser.add_argument('-bk', '--konqueror', action='store_true')   # browse with konqueror
-    parser.add_argument('-bo', '--xdg-open',  action='store_true')   # browse with xdg-open - beware of infinte recursion xdg-open may decide to use this srcipt !
-    parser.add_argument('-nl', '--nolog',     action='store_true')   # log (append) to given log file
+    parser.add_argument('file|folder path')                           # positional argument
+    parser.add_argument('-t', '--type',       action='store_true')    # type out the file containing the url
+    parser.add_argument('-u', '--url',        action='store_true')    # print the url
+    parser.add_argument('-v', '--verbose',    action='store_true')    # be verbose
+    parser.add_argument('-bf', '--firefox',   action='store_true')    # browse with firefox
+    parser.add_argument('-bc', '--chromium',  action='store_true')    # browse with chromium
+    parser.add_argument('-bk', '--konqueror', action='store_true')    # browse with konqueror
+    parser.add_argument('-bo', '--xdg-open',  action='store_true')    # browse with xdg-open - beware of infinte recursion xdg-open may decide to use this srcipt !
+    parser.add_argument('-nl', '--noLog',     action='store_true')    # log (append) to given log file
 
-    args = parser.parse_args()                                       # print(args.filename, args.verbose)
+    args = parser.parse_args()                                        # print(args.filename, args.verbose)
+    pth = vars(args)['file|folder path']
 
-    # pthType = None
-    # if os.path.exists(pathname):
-    #     if os.path.isfile(pathname):  pthType='file'
-    #     elif os.path.isdir(pathname): pthType='folder'
-    #     else any(char in s for path in '*?[]'):
-    #         print(f"{pathname} exists, but is neither a file nor a folder.")
-    #         sys.exit(1)
-    # else:
-    #     print(f"{pathname} does not exist.")
-    #     sys.exit(1)
-
-
-    if args.type:
-        for l in open(args.filename): print(l, end='')
-    elif args.url:
-        url = get_url_file(args.filename)
-        print("URL=", url)
+    pthType = None
+    if os.path.isfile(pathname):
+        pthType='file'
+        files = [pth]
+    elif os.path.isdir(pathname):
+        pthType='folder'
+        files = glob(pth)
+    elif any(c in pth for c in '*?[]'):                               # is it a glob expression
+        files = glob(pth)
     else:
-        url = get_url_file(args.filename)
-        print("URL=", url)
-        subprocess.run([chosenBrowser, url], check=True, stderr=open(stderrFile, 'w'), text=True)
-        if True or not args.noLog:
-            bh = open(browseLogFile, 'a')
-            print(f"DATE={dt}, URL={url}", file=bh)
-            print(f"DATE={dt}, URL={url}")
+        print(f"failed to interpret {pth}")
+        sys.exit(1)
+
+    for fl in files:
+        if args.type:                                                 # type out the contents of the file
+            print("---",fl)
+            for l in open(fl): print(l, end='')
+        elif args.url:                                                # print the embedded url
+            print("---",fl)
+            print("URL=", get_url_file(fl))
+        else:
+            url = get_url_file(fl)
+            print(fl, ":URL=", url)
+            subprocess.run([chosenBrowser, url], check=True, stderr=open(stderrFile, 'w'), text=True)
+            if not args.noLog:
+                bh = open(browseLogFile, 'a')
+                print(f"DATE={dt}, URL={url}", file=bh)
+                print(f"DATE={dt}, URL={url}")
 
 # TBD inspect first lines for:  "<!doctype html><html..."
 # TBD - if it's a folder open all bookmark files in the folder!
