a775cfcc04de432cabceeb880222c8350e5d4d58
Mike Kleyn <michfkl@btinternet.com>
2024-02-14 21:36:35 +0000

    not only file-path, but also folder-path as cmd line argument, i.e. open urls in all files in folder

--- openUrlFile.py
+++ openUrlFile.py
@@ -30,8 +30,11 @@ def get_url_file(file_path):
     ext = ext.lower()
     url = None
 
-    if ext=='.url':
+    if ext=='.url':  # check for sections, without sections, configparser doesn't work
         config = configparser.ConfigParser()
+        if args.verbose:
+            #for k,v in config.items(): print('config',k,v)
+            print(config)
         config.read(file_path)
         url = config.get('InternetShortcut', 'URL')
     elif ext=='.desktop':
@@ -42,6 +45,8 @@ def get_url_file(file_path):
     elif ext=='.webloc':
         with open(file_path, 'rb') as f: url = plistlib.load(f).get('URL')
     elif ext=='.html':
+        # an html file with one url in it works automaticaly with firefox
+        # e.g. <html><head><meta http-equiv="refresh" content="0; url=https://www.youtube.com/watch?v=jWkWD1MBXKU" /></head></html>
         url = file_path                                                         # just pass the file path to the browser
     else:
         print(f'Unsupported file type: {ext}')
@@ -55,17 +60,29 @@ if __name__ == "__main__":
         description='open url embedded in file',
         epilog='-------- openUrlFile.py --------')
 
-    parser.add_argument('filename')                                  # positional argument
+    parser.add_argument('file|folder path')                       # positional argument
     parser.add_argument('-t', '--type',       action='store_true')   # type out the file containing the url
     parser.add_argument('-u', '--url',        action='store_true')   # print the url
     parser.add_argument('-v', '--verbose',    action='store_true')   # be verbose
     parser.add_argument('-bf', '--firefox',   action='store_true')   # browse with firefox
-    parser.add_argument('-bc', '--chromium',  action='store_true')   # browse with chromium
+    parser.add_argument('-bc', '--chromium',  action='store_true')   # browse with chromiumT
     parser.add_argument('-bk', '--konqueror', action='store_true')   # browse with konqueror
     parser.add_argument('-bo', '--xdg-open',  action='store_true')   # browse with xdg-open - beware of infinte recursion xdg-open may decide to use this srcipt !
     parser.add_argument('-nl', '--nolog',     action='store_true')   # log (append) to given log file
 
     args = parser.parse_args()                                       # print(args.filename, args.verbose)
+
+    # pthType = None
+    # if os.path.exists(pathname):
+    #     if os.path.isfile(pathname):  pthType='file'
+    #     elif os.path.isdir(pathname): pthType='folder'
+    #     else any(char in s for path in '*?[]'):
+    #         print(f"{pathname} exists, but is neither a file nor a folder.")
+    #         sys.exit(1)
+    # else:
+    #     print(f"{pathname} does not exist.")
+    #     sys.exit(1)
+
 
     if args.type:
         for l in open(args.filename): print(l, end='')
@@ -76,9 +93,12 @@ if __name__ == "__main__":
         url = get_url_file(args.filename)
         print("URL=", url)
         subprocess.run([chosenBrowser, url], check=True, stderr=open(stderrFile, 'w'), text=True)
-        if not args.noLog:
+        if True or not args.noLog:
             bh = open(browseLogFile, 'a')
             print(f"DATE={dt}, URL={url}", file=bh)
+            print(f"DATE={dt}, URL={url}")
 
-
+# TBD inspect first lines for:  "<!doctype html><html..."
 # TBD - if it's a folder open all bookmark files in the folder!
+# See also https://benchdoos.github.io/
+# https://gitlab.com/claderoki/QuickCut
